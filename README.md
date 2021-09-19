7e551c60d4ce9010c7acfc789ea91d805ba010a2
vsoch <vsoch@users.noreply.github.com>
2021-09-18 22:18:37 -0600

    adding first demo with some functionality
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- README.md
+++ README.md
@@ -4,3 +4,30 @@ Welcome to the Candy Generator! This is a tool that will allow you to create
 and save your own repository candy, to truly celebrate an Open Source Halloween! 
 
 🚧️ **under development** 🚧️
+
+## Frequently Asked Questions
+
+### How does it work?
+
+Overall, the idea is that we are putting together puzzle pieces. We have candy form bases (e.g., an outline of a candy bar)
+that we can add to an svg, and then further customize by adding text, textures, and color. 
+
+### How did I make the candy backgrounds?
+
+This was a manual process! I started with a png of a candy that I wanted to use, and then:
+
+1. In Inkscape used trace bitmap with 8 colors to get to a bottom, solid layer
+2. Added an extra layer of the same color (usually a large rectangle) and combined with the lower to fill in gaps.
+3. Saved to an svg file, stored in [assets/candy](assets/candy)
+4. Copied the paths, style, and other attributes into the [assets/js/candy.json](assets/js/candy.json)
+
+Getting the text to line up was a big of an art! I tested each background to make a custom scale
+and then x and y offset for the main text.
+
+## Thanks
+
+The Candy Generator would not be possible without these amazing supporting libraries!
+
+- [textures](https://riccardoscalco.it/textures/): to make your candy wrapper textures!
+- [d3](https://d3js.org/): for easily mapping data to svg objects
+- [jquery](https://jquery.com/): because you can make a million new trendy JS libraries but this one always is easy-peezy
--- assets/js/candygen.js
+++ assets/js/candygen.js
@@ -1,10 +1,7 @@
 
 // Candy Generator!
-// Requires JQuery and D3.
+// Requires JQuery and D3 and a candy.json!
 
-var candy = {"candybar": {"paths": Array("M 357.85362,254.60232 C 353.45057,252.93429 21.078496,252.66671 -0.29183729,254.314 -9.2381626,255.0036 -12.17978,254.78916 -13.625171,253.34201 c -1.533659,-1.53552 -1.854543,-13.23294 -1.854543,-67.60517 v -65.74841 l 180.235924,0.26383 c 161.18977,0.23594 180.66192,0.0435 184.26712,-1.82079 5.02251,-2.59723 23.18071,-2.89862 25.18108,-0.41795 0.8834,1.0955 0.96263,9.20618 0.23121,23.66667 -0.61202,12.1 -1.14336,42.48582 -1.18072,67.52402 -0.0601,40.24162 -0.31266,45.61792 -2.177,46.33334 -3.09164,1.18637 -8.66422,0.79228 -13.22428,-0.93523 z"),
-"attrs": [["fill", "#401d02"], ["stroke-width", 1.33333337], ["fill-opacity", 1]],
-"transform": "translate(21.002386,1.9093079)"}}
 
 class CandyGen {
 
@@ -13,26 +10,29 @@ class CandyGen {
 
     // Keep record of candy bases!
     items = {
-        templates: Object.keys(candy),
+        templates: [],
         choices: {}
     };
 
     // The constructor is called when we create a Candy Gen object
-    constructor(width, height, divid, resetButton) {
+    constructor(candy, width, height, divid, resetButton, formButton) {
 
         // Choose a random template to start with
-        this.chooseTemplate()
         this.width = width || 800;
-        this.height = height || 800;
+        this.height = height || 500;
         this.divid = divid || "#candy";
+
+        // Loaded candy json
+        this.candy = candy;
+        this.items.templates = Object.keys(this.candy);
 
         // Button identifiers
         this.resetButton = resetButton || "#resetCandy"
+        this.textureButton = resetButton || "#candy-texture"
+        this.formButton = formButton || "#candy-form"
 
         // Variables for the svg, and candy image svg within
-        this.svg = null;
-        this.img = null;
-        this.setTemplate();
+        this.reset();
 
         // Setup color choosers
         this.setupChoosers()        
@@ -42,6 +42,8 @@ class CandyGen {
 
         // Event binding for buttons, setup of color picker
         $(this.resetButton).on("click", {client: this}, this.reset);
+        $(this.textureButton).on("change", {client: this}, this.changeTexture);
+        $(this.formButton).on("change", {client: this}, this.changeForm);
      
     }
 
@@ -51,17 +53,138 @@ class CandyGen {
     }
 
     // Choose a template, either randomly or by choice
-    chooseTemplate(choice) {
+    chooseTemplate(choice, client) {
     
+        client = client || this
+                    
         // If a choice is provided and it is in the array
-        if (choice != null && this.items.templates.include(choice)) {
+        if (choice != null && client.items.templates.includes(choice)) {
+            var choice = client.items.choices["template"] = this.candy[choice];  
             this.items.choices["template"] = choice; 
-        
+            
         // Otherwies choose randomly
         } else {
-            var index = Math.floor(Math.random() * this.items.templates.length);
-            this.items.choices["template"] = candy[this.items.templates[index]];  
+            var index = Math.floor(Math.random() * client.items.templates.length);
+            client.items.choices["template"] = this.candy[client.items.templates[index]];  
         }
+    }
+  
+    // Set the candy texture
+    changeTexture(event) {
+        var client = event.data.client
+        var texture = $(this).val();
+        client.setTexture(texture, client)    
+    }
+
+    // Change the candy form (background)
+    changeForm(event) {
+        var client = event.data.client
+        var form = $(this).val();
+        client.chooseTemplate(form, client)
+        client.setTemplate()    
+    }
+       
+    // Actually set a texture
+    setTexture(texture, client) {
+
+        // If no client provided, use this
+        client = client || this
+
+        // Default texture is current
+        texture = texture || client.items.choices['texture']
+
+        // If solid texture, set fill to selected color
+        if (texture == "solid") {
+             console.log("updating texture")
+             client.paths.style("fill", client.items.choices['color']); 
+
+        // Diagonal lines
+        } else if (texture == "lines") {
+           var t = textures.lines()
+                   .orientation("diagonal")
+                   .size(40)
+                   .strokeWidth(26)
+                   .stroke(client.items.choices["color"])
+                   .background(client.items.choices['texture_color']);
+           client.svg.call(t);
+           client.paths.style("fill", t.url());
+
+        } else if (texture == "checkers") {
+           var t = textures.lines()
+                   .orientation("3/8", "7/8")
+                   .stroke(client.items.choices["texture_color"])
+                   .background(client.items.choices['color']);
+           client.svg.call(t);
+           client.paths.style("fill", t.url());         
+
+        } else if (texture == "circles") {
+           var t = textures.circles()
+                   .fill(client.items.choices["texture_color"])
+                   .background(client.items.choices['color']);
+           client.svg.call(t);
+           client.paths.style("fill", t.url());         
+
+        } else if (texture == "patch") {
+           var t = textures.circles()
+                   .complement()
+                   .fill(client.items.choices["texture_color"])
+                   .background(client.items.choices['color']);
+           client.svg.call(t);
+           client.paths.style("fill", t.url());         
+
+        } else if (texture == "honeycomb") {
+           var t = textures.paths()
+                   .d("hexagons")
+                   .size(8)
+                   .strokeWidth(2)
+                   .background(client.items.choices['color'])
+                   .stroke(client.items.choices["texture_color"]);
+           client.svg.call(t);
+           client.paths.style("fill", t.url());         
+
+        } else if (texture == "caps" || texture == "crosses" || texture == "woven") {
+           var t = textures.paths()
+                   .d(texture)
+                   .lighter()
+                   .thicker()  
+                   .strokeWidth(2)
+                   .background(client.items.choices['color'])
+                   .stroke(client.items.choices["texture_color"]);
+           client.svg.call(t);
+           client.paths.style("fill", t.url());                         
+
+        } else if (texture == "waves") {
+           var t = textures.paths()
+                   .d("waves")
+                   .thicker()
+                   .strokeWidth(2)
+                   .background(client.items.choices['color'])
+                   .stroke(client.items.choices["texture_color"]); 
+           client.svg.call(t);
+           client.paths.style("fill", t.url());                                                   
+
+        } else if (texture == "nylon") {
+           var t = textures.paths()
+                   .d("nylon")
+                   .lighter()
+                   .strokeWidth(2)
+                   .shapeRendering("crispEdges")
+                   .background(client.items.choices['color'])
+                   .stroke(client.items.choices["texture_color"]); 
+           client.svg.call(t);
+           client.paths.style("fill", t.url());                                                   
+
+        } else if (texture == "squares") {
+           var t = textures.paths()
+                   .d("squares")
+                   .thicker()
+                   .strokeWidth(2)
+                   .background(client.items.choices['color'])
+                   .stroke(client.items.choices["texture_color"]); 
+           client.svg.call(t);
+           client.paths.style("fill", t.url());                                                   
+        }
+        client.items.choices['texture'] = texture;
     }
 
     setupChoosers() {
@@ -78,9 +201,18 @@ class CandyGen {
             console.log("Updating color to " + event.target.value);
             d3.select("#candy-path").attr("fill", event.target.value);
             client.items.choices["color"] = event.target.value;
+            client.setTexture(client.items.choices["texture"], client)
         }       
+        function updateTexture (event) {
+            console.log("Updating texture color to " + event.target.value);
+            client.items.choices["texture_color"] = event.target.value;
+            client.setTexture(client.items.choices["texture"], client)
+        }
+
         this.colorChooser = document.querySelector("#candy-color");
         this.colorChooser.addEventListener("change", updateColor, false);        
+        this.textureColorChooser = document.querySelector("#texture-color");
+        this.textureColorChooser.addEventListener("change", updateTexture, false);        
     }
 
     // set the chosen template
@@ -103,19 +235,23 @@ class CandyGen {
         this.paths = this.group.append('svg:path')
           .attr('d', function(d) {return d.paths})
           .attr('id', "candy-path")
-          .attr('transform', "scale(2)")
+          .attr('transform', function(d) {return "scale("+ d.scale + ")"})
 
           // Set any attributes provided by default
           .each(function (d) {
               for (var attr of d.attrs) {
-                  d3.select(this).attr(attr[0], attr[1])}
+                  d3.select(this).attr(attr[0], attr[1])
+                     if (attr[0] == "fill") {
+                       d3.select("#candy-color").value = attr[1];                      
+                     }
+                  }
               })
 
          // TODO: this will load a repository name for a candy
-         // TODO: need to have backgrounds / textures / "nutrition facts" based on repository metadata
+         // TODO: "nutrition facts" based on repository metadata
          this.group.append("text")
-           .attr("x", function(d) { var center = getTextLocation(d3.select("#candy-path")); return center[0]; })
-           .attr("y", function(d) { var center = getTextLocation(d3.select("#candy-path")); return center[1]; })
+           .attr("x", function(d) { var center = getTextLocation(d3.select("#candy-path"), d.xoffset, d.yoffset); return center[0]; })
+           .attr("y", function(d) { var center = getTextLocation(d3.select("#candy-path"), d.xoffset, d.yoffset); return center[1]; })
            .attr("fill", "white")
            .attr("font-size", 26)
            .text(function(d) { return "HAPPY HALLOWEEN!"; });
@@ -123,16 +259,23 @@ class CandyGen {
 
     // Reset Choices and Chandy
     reset(event) {
-        var client = event.data.client
-        client.items.choices = {};
-        client.chooseTemplate();
+        var client;
+        if (event != null) {client = event.data.client} else {client = this}
+        this.svg = null;
+        this.img = null;
+        client.items.choices = {"texture": "solid", "texture_color": "green", "color": "purple"};
+        client.chooseTemplate("hersheys");
         client.setTemplate();
     }
 }
 
 // Helper functions
-function getTextLocation (selection) {
+function getTextLocation (selection, xoffset, yoffset) {
   var element = selection.node();
   var bbox = element.getBBox();
-  return [bbox.x + 50, bbox.y + bbox.height + 50];
+  return [bbox.x + xoffset, bbox.y + yoffset];
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
